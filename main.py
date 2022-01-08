58ee2b8db068084d0bf146ba49b4239a68aa6b95
Hee Hwang <heehcs@gmail.com>
2022-01-08 13:51:00 -0500

    Implement minimal replication

--- main.py
+++ main.py
@@ -11,20 +11,24 @@ print(input_details)
 print(output_details)
 
 input_shape = input_details[0]['shape']
-im = cv2.imread("buggy_image_2.jpg")
+im = cv2.imread("buggy_image_2.jpg")  # 1920x1080
+
+# RGB conversion
 im_rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-im_rgb = cv2.resize(im_rgb, (input_shape[1], input_shape[2]))
+
+# Crop center
+crop_im = im[0:1080, 420:1500]  # 1080x1080
+
+# Resize
+im_rgb = cv2.resize(im_rgb, (input_shape[1], input_shape[2]))  # 300x300
+
+# Set dimension
 input_data = np.expand_dims(im_rgb, axis=0)
 print(input_data.shape)
-
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
+# Inference
 interpreter.invoke()
-
-#output_data = interpreter.get_tensor(output_details[0]['index'])
-
-img_width=1920
-img_height=1080
 
 detection_boxes = interpreter.get_tensor(output_details[0]['index'])
 detection_classes = interpreter.get_tensor(output_details[1]['index'])
@@ -44,15 +48,23 @@ print("==num_boxes==")
 print(num_boxes)
 print()
 
+# Rendering
+# img_width=1080
+# img_height=1080
 # for i in range(int(num_boxes[0])):
 #   if detection_scores[0, i] > .5:
 #        x = detection_boxes[0, i, [1, 3]] * img_width
 #        y = detection_boxes[0, i, [0, 2]] * img_height
 #        rectangle = [x[0], y[0], x[1], y[1]]
 #        class_id = detection_classes[0, i]
-
-
-
 #print(output_data.shape)
 #print()
 #print(output_data)
+
+# Show image
+# cv2.imshow("", crop_im)
+# wait = True
+# while wait:
+#   wait = cv2.waitKey()=='q113' # hit q to exit
+
+#output_data = interpreter.get_tensor(output_details[0]['index'])
